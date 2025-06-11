bd3c4a545782d89d0e9d6431166716c4f4626d48
Caluk55 <32982931+Caluk55@users.noreply.github.com>
2025-06-11 19:10:23 +0200

    Add files via upload

--- ComponentTesterESP32S3.ino
+++ ComponentTesterESP32S3.ino
@@ -12,6 +12,7 @@
 #include "ADC.h"
 #include "PWM.h"
 #include "probes.h"
+#include "tp.h
 
 TFT_eSPI tft = TFT_eSPI();  // Oggetto globale del display
 
@@ -19,6 +20,7 @@ void setup() {
   Serial.begin(115200);
   delay(100);
 
+  tp.begin()
   initConfig();
   initDisplay(&tft);
   showSplash();
@@ -26,6 +28,7 @@ void setup() {
   initADC();
   initPWM();
   initProbes();
+  
 
   initCommands();
   initUser();
@@ -34,4 +37,5 @@ void setup() {
 void loop() {
   commandsLoop();  // Gestione automatica
   userLoop();      // Eventuale interazione manuale
+  tp.runTestCycle()
 }
