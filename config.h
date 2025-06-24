d6e8cadda5499ab47eed824b1ba31eab4b853d62
Caluk55 <32982931+Caluk55@users.noreply.github.com>
2025-06-24 18:42:27 +0200

    Update config.h

--- config.h
+++ config.h
@@ -17,9 +17,9 @@
 #define GPIO_STEPUP_EN  25  // Step Up Enable (Zener)
 
 // === Pin Encoder / Pulsante ===
-#define BUTTON_UP_PIN    15
-#define BUTTON_DOWN_PIN  16
-#define BUTTON_SELECT_PIN 17
+#define BUTTON_UP_PIN    1
+#define BUTTON_DOWN_PIN  2
+#define BUTTON_SELECT_PIN 3
 
 // === ADC Settings ===
 #define ADC_RESOLUTION 4095.0f  // 12-bit
